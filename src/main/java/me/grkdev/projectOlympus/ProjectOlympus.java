7f49043354a16796e68a795f5d8ca10debe810d3
GrkDev <thedevgrk@gmail.com>
2024-11-04 23:59:42 -0600

    Init Project

---
+++ src/main/java/me/grkdev/projectOlympus/ProjectOlympus.java
@@ -0,0 +1,13 @@
+package me.grkdev.projectOlympus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ProjectOlympus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+}
